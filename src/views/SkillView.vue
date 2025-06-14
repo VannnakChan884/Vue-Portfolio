194a16eff8245abe0fced62fab0d6b7d73d5d141
Chan Vannak <vannakchan884@gmail.com>
2025-06-14 12:39:09 +0700

    add skills

---
+++ src/views/SkillView.vue
@@ -0,0 +1,57 @@
+<template>
+    <!-- Skills Section -->
+        <section id="skills" class="py-28 bg-white dark:bg-gray-800">
+            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200 text-center mb-12 uppercase">skills_title</h2>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    <!-- Design Skills -->
+                    <div class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-md">
+                        <div class="flex items-center mb-4">
+                            <div class="bg-blue-100 p-3 rounded-full mr-4">
+                                <i class="fa-solid fa-palette text-accent text-xl"></i>
+                            </div>
+                            <h3 class="text-xl font-semibold text-primary dark:text-white">design_skills</h3>
+                        </div>
+                        <div class="space-y-3">
+                            <div class="flex items-center">
+                                <div class="w-2 h-2 bg-accent rounded-full mr-3"></div>
+                                <span class="text-gray-600 dark:text-white">design_skills</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- IT Skills -->
+                     <div class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-md">
+                        <div class="flex items-center mb-4">
+                            <div class="bg-blue-100 p-3 rounded-full mr-4">
+                                <i class="fa-solid fa-network-wired text-accent text-xl"></i>
+                            </div>
+                            <h3 class="text-xl font-semibold text-primary dark:text-white">it_skills</h3>
+                        </div>
+                        <div class="space-y-3">
+                            <div class="flex items-center">
+                                <div class="w-2 h-2 bg-accent rounded-full mr-3"></div>
+                                <span class="text-gray-600 dark:text-white">it_skills</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Coding Skills -->
+                     <div class="bg-white dark:bg-gray-700 p-6 rounded-lg shadow-md">
+                        <div class="flex items-center mb-4">
+                            <div class="bg-blue-100 p-3 rounded-full mr-4">
+                                <i class="fa-solid fa-code text-accent text-xl"></i>
+                            </div>
+                            <h3 class="text-xl font-semibold text-primary dark:text-white">code_skills</h3>
+                        </div>
+                        <div class="space-y-3">
+                            <div class="flex items-center">
+                                <div class="w-2 h-2 bg-accent rounded-full mr-3"></div>
+                                <span class="text-secontext-gray-600 dark:text-white">code_skills</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
