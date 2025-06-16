476a170684eada6a469b28f193c9ada5584effaf
HGOF366 <vannak@kh.handseven.com>
2025-06-16 13:04:31 +0700

    custom contact form

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- Contact Section -->
-    <div class="absolute top-0 right-0 py-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 bg-white dark:bg-gray-900 px-6">
+    <div class="absolute top-0 right-0 py-6 pb-24 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 bg-white dark:bg-gray-900 px-6">
         <div class="md:ml-6 lg:ml-6 xl:ml-6">
-            <div class="mb-12">
+            <div class="max-w-4xl mx-auto mb-12">
                 <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200 text-center">Get In Touch</h2>
                 <p class="max-w-full mx-auto mt-2 mb-4 text-center text-sm text-gray-500 dark:text-gray-300">I'll create
                     high-quality linkable content and build at least 40 high-authority links to each asset, paving the
@@ -10,35 +10,66 @@
                     for you to grow your rankings, improve brand.</p>
             </div>
             <div
-                class="max-w-6xl mx-auto bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 p-3 rounded-2xl shadow-md">
+                class="max-w-4xl mx-auto bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 p-3 rounded-2xl shadow-md">
                 <div class="grid md:grid-cols-2 gap-x-6 gap-y-6">
-                    <div class="relative bg-gray-100 dark:bg-gray-900 p-8 rounded-lg overflow-hidden">
-                        <h4 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Contact
+                    <div class="relative bg-gray-100 dark:bg-gray-900 p-6 rounded-lg overflow-hidden">
+                        <h4 class="text-xl font-bold text-gray-800 dark:text-gray-200">Contact
                             Information</h4>
                         <p class="max-w-full mx-auto mt-2 mb-4 text-justify text-sm text-gray-500 dark:text-gray-300">
                             I'll create high-quality linkable content and build at least 40 high-authority.</p>
-                        <div class="w-64 h-64 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -bottom-20 -left-20 rounded-full"></div>
-                        <div class="w-48 h-48 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -top-20 -right-20 rounded-full"></div>
+                        <div
+                            class="w-64 h-64 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -bottom-20 -left-20 rounded-full">
+                        </div>
+                        <div
+                            class="w-48 h-48 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -top-20 -right-20 rounded-full">
+                        </div>
+                        <div class="flex flex-col md:space-y-4 mt-6">
+                            <div class="flex items-center">
+                                <div class="w-10 h-10 flex items-center justify-start">
+                                    <i class="fa-solid fa-phone text-lg"></i>
+                                </div>
+                                <span class="text-sm">+855 96 26 65 240<br>+855 88 600 4544</span>
+                            </div>
+                            <div class="flex items-center">
+                                <div class="w-10 h-10 flex items-center justify-start">
+                                    <i class="fa-solid fa-envelope text-lg"></i>
+                                </div>
+                                <a href="#" class="text-sm">vannakchan884@gmail.com</a>
+                            </div>
+                            <div class="flex items-center">
+                                <div class="w-10 h-10 flex items-center justify-start">
+                                    <i class="fa-solid fa-location-dot text-lg"></i>
+                                </div>
+                                <span class="text-sm">Takeo, Cambodia</span>
+                            </div>
+                        </div>
                     </div>
                     <form>
-                        <div class="mb-6">
-                            <label for="name" class="text-sm font-medium mb-1">Name</label>
-                            <input type="text" required
-                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
+                        <div class="grid lg:grid-cols-2 gap-x-6">
+                            <div class="mb-6">
+                                <label for="name" class="text-sm font-medium mb-1">Your Name</label>
+                                <input type="text" required placeholder="Your name here"
+                                    class="w-full pb-2 bg-transparent dark:bg-gray-700 dark:border-gray-600 border-b border-gray-300 focus:border-gray-500 dark:focus:border-gray-200 outline-none">
+                            </div>
+                            <div class="mb-6">
+                                <label for="email" class="text-sm font-medium mb-1">
+                                    Your Email
+                                </label>
+                                <input type="email" required placeholder="example@gmail.com"
+                                    class="w-full pb-2 bg-transparent dark:bg-gray-700 dark:border-gray-600 border-b border-gray-300 focus:border-gray-500 dark:focus:border-gray-200 outline-none">
+                            </div>
                         </div>
                         <div class="mb-6">
-                            <label for="email" class="text-sm font-medium mb-1">
-                                Email
-                            </label>
-                            <input type="email" required
-                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
+                            <label for="subject" class="text-sm font-medium mb-1">Your Subject</label>
+                            <input type="text" required placeholder="Your subject here"
+                                class="w-full pb-2 bg-transparent dark:bg-gray-700 dark:border-gray-600 border-b border-gray-300 focus:border-gray-500 dark:focus:border-gray-200 outline-none">
                         </div>
                         <div class="mb-6">
                             <label for="message" class="text-sm font-medium mb-1">
-                                Message
+                                Your Message
                             </label>
-                            <textarea rows="5" required
-                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"></textarea>
+                            <textarea rows="4" required placeholder="Your message here"
+                                class="w-full pb-2 bg-transparent dark:bg-gray-700 dark:border-gray-600 border-b border-gray-300 focus:border-gray-500 dark:focus:border-gray-200 outline-none"></textarea>
                         </div>
                         <div>
                             <button type="submit"
