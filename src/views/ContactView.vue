ebe54924a1d031c0100d34f2e819a6f08de05e98
HGOF366 <vannak@kh.handseven.com>
2025-06-16 10:59:54 +0700

    modify contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,39 +1,53 @@
 <template>
     <!-- Contact Section -->
-    <div
-        class="absolute top-0 right-0 py-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 bg-white dark:bg-gray-900 px-6">
+    <div class="absolute top-0 right-0 py-6 w-full md:w-3/4 lg:w-4/5 xl:w-5/6 bg-white dark:bg-gray-900 px-6">
         <div class="md:ml-6 lg:ml-6 xl:ml-6">
-            <div class="max-w-2xl mx-auto bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 p-6 rounded-2xl shadow-md">
-                <form class="space-y-6">
-                    <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200 text-center uppercase">contact_title</h2>
-                    <div>
-                        <label for="name" class="text-sm font-medium mb-1">
-                            name
-                        </label>
-                        <input type="text" required
-                            class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
+            <div class="mb-12">
+                <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200 text-center">Get In Touch</h2>
+                <p class="max-w-full mx-auto mt-2 mb-4 text-center text-sm text-gray-500 dark:text-gray-300">I'll create
+                    high-quality linkable content and build at least 40 high-authority links to each asset, paving the
+                    way
+                    for you to grow your rankings, improve brand.</p>
+            </div>
+            <div
+                class="max-w-6xl mx-auto bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-300 p-3 rounded-2xl shadow-md">
+                <div class="grid md:grid-cols-2 gap-x-6 gap-y-6">
+                    <div class="relative bg-gray-100 dark:bg-gray-900 p-8 rounded-lg overflow-hidden">
+                        <h4 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Contact
+                            Information</h4>
+                        <p class="max-w-full mx-auto mt-2 mb-4 text-justify text-sm text-gray-500 dark:text-gray-300">
+                            I'll create high-quality linkable content and build at least 40 high-authority.</p>
+                        <div class="w-64 h-64 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -bottom-20 -left-20 rounded-full"></div>
+                        <div class="w-48 h-48 bg-gray-400/30 dark:bg-gray-600/40 blur-2xl absolute -top-20 -right-20 rounded-full"></div>
                     </div>
-                    <div class="mb-6">
-                        <label for="email" class="text-sm font-medium mb-1">
-                            email
-                        </label>
-                        <input type="email" required
-                            class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
-                    </div>
-                    <div class="mb-6">
-                        <label for="message" class="text-sm font-medium mb-1">
-                            message
-                        </label>
-                        <textarea rows="5" required
-                            class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"></textarea>
-                    </div>
-                    <div>
-                        <button type="submit"
-                            class="w-full bg-gray-800 hover:bg-blue-700 dark:bg-gray-900 dark:hover:bg-gray-800 text-gray-200 font-medium py-3 px-4 rounded-md btn">
-                            submit
-                        </button>
-                    </div>
-                </form>
+                    <form>
+                        <div class="mb-6">
+                            <label for="name" class="text-sm font-medium mb-1">Name</label>
+                            <input type="text" required
+                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
+                        </div>
+                        <div class="mb-6">
+                            <label for="email" class="text-sm font-medium mb-1">
+                                Email
+                            </label>
+                            <input type="email" required
+                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent">
+                        </div>
+                        <div class="mb-6">
+                            <label for="message" class="text-sm font-medium mb-1">
+                                Message
+                            </label>
+                            <textarea rows="5" required
+                                class="w-full px-4 py-2 border border-gray-300 rounded-md dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"></textarea>
+                        </div>
+                        <div>
+                            <button type="submit"
+                                class="w-full bg-gray-800 hover:bg-blue-700 dark:bg-gray-900 dark:hover:bg-gray-800 text-gray-200 font-medium py-3 px-4 rounded-md btn">
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
