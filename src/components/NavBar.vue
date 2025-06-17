b144cd01664f4a27b2f3617d010535ad066a4c1d
HGOF366 <vannak@kh.handseven.com>
2025-06-17 11:28:12 +0700

    change width navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,13 +16,13 @@ const toggleMenu = () => {
 <template>
   <header class="w-full md:h-[96vh] md:w-1/4 lg:w-1/5 xl:w-1/6 fixed md:top-4 bottom-0 md:p-4 z-50">
     <div
-      class="w-full md:h-full p-2 md:p-4 flex flex-col absolute bottom-0 backdrop-blur-sm bg-gray-200/70 dark:bg-gray-800/70 rounded-t-2xl shadow-xl shadow-gray-400/30 md:rounded-lg transition-all duration-300 ease-in-out"
-      :class="{ 'h-16 md:w-32': !isOpen, 'h-auto md:w-56': isOpen }">
+      class="w-full md:h-full p-2 md:p-4 flex flex-col absolute bottom-0 backdrop-blur-sm bg-gray-200/70 dark:bg-gray-800/70 rounded-t-2xl md:rounded-lg transition-all duration-300 ease-in-out"
+      :class="{ 'h-14 md:w-32': !isOpen, 'h-auto md:w-56': isOpen }">
       <!-- Mobile Header (always visible on mobile) -->
       <div class="md:hidden flex justify-between items-center mb-2">
         <h1 class="text-xl text-gray-700 dark:text-white/80 font-bold">Vannak</h1>
         <button @click="toggleMenu"
-          class="w-12 h-12 p-2 flex justify-center items-center hover:bg-gray-700 hover:dark:bg-white/80 hover:text-gray-200 text-gray-700 dark:text-white/80 hover:dark:text-gray-700 rounded-full transition duration-300 ease-in-out">
+          class="md:w-12 md:h-12 p-2 flex justify-center items-center hover:bg-gray-700 hover:dark:bg-white/80 hover:text-gray-200 text-gray-700 dark:text-white/80 hover:dark:text-gray-700 rounded-full transition duration-300 ease-in-out">
           <i class="fa-solid" :class="isOpen ? 'fa-xmark' : 'fa-bars-staggered'"></i>
         </button>
       </div>
@@ -32,9 +32,11 @@ const toggleMenu = () => {
         <h1 class="text-xl md:text-4xl text-gray-700 dark:text-white/80 font-bold transition-all duration-300 ease-in"
           :class="{ 'hidden': !isOpen, 'flex': isOpen }">Vannak
         </h1>
-        <img src="../assets/avatar.jpg" alt="Avatar" class="w-12 h-12 object-cover rounded-full transition duration-300 ease-in-out" :class="{ 'flex': !isOpen, 'hidden': isOpen }">
+        <img src="../assets/avatar.jpg" alt="Avatar"
+          class="md:w-12 md:h-12 object-cover rounded-lg transition duration-300 ease-in-out"
+          :class="{ 'flex': !isOpen, 'hidden': isOpen }">
         <button @click="toggleMenu"
-          class="w-8 h-8 p-2 flex justify-center items-center hover:bg-gray-700 hover:dark:bg-white/80 hover:text-gray-200 text-gray-700 dark:text-white/80 hover:dark:text-gray-700 rounded-full transition duration-300 ease-in-out">
+          class="w-8 h-8 p-2 flex justify-center items-center hover:bg-gray-700/20 hover:dark:bg-gray-700/50 text-gray-700 dark:text-gray-200 rounded-lg transition duration-300 ease-in-out">
           <i class="fa-solid" :class="isOpen ? 'fa-chevron-left' : 'fa-chevron-right'"></i>
         </button>
       </div>
@@ -46,36 +48,45 @@ const toggleMenu = () => {
           <RouterLink to="/"
             class="h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex flex-col md:flex-row items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <i
-              class="fa-solid fa-house text-green-800 dark:text-green-500 text-base md:text-3xl lg:text-4xl xl:text-5xl"></i>
+            <span class="md:w-12 md:h-12 flex items-center justify-center">
+              <i class="fa-solid fa-house text-green-800 dark:text-green-500 text-base md:text-3xl lg:text-4xl"></i>
+            </span>
             <span class="text-[7px] font-bold md:text-base lg:text-md xl:text-lg md:ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">Home</span>
           </RouterLink>
           <RouterLink to="/about"
             class="h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex flex-col md:flex-row items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <i class="fa-solid fa-user text-base md:text-3xl lg:text-4xl xl:text-5xl"></i>
+            <span class="md:w-12 md:h-12 flex items-center justify-center">
+              <i class="fa-solid fa-user text-base md:text-3xl lg:text-4xl"></i>
+            </span>
             <span class="text-[7px] md:text-base lg:text-md xl:text-lg md:ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">About</span>
           </RouterLink>
           <RouterLink to="/skill"
             class="h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex flex-col md:flex-row items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <i class="fa-solid fa-pen-nib text-base md:text-3xl lg:text-4xl xl:text-5xl"></i>
+            <span class="md:w-12 md:h-12 flex items-center justify-center">
+              <i class="fa-solid fa-pen-nib text-base md:text-3xl lg:text-4xl"></i>
+            </span>
             <span class="text-[7px] md:text-base lg:text-md xl:text-lg md:ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">Skills</span>
           </RouterLink>
           <RouterLink to="/project"
             class="h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex flex-col md:flex-row items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <i class="fa-solid fa-list-check text-base md:text-3xl lg:text-4xl xl:text-5xl"></i>
+            <span class="md:w-12 md:h-12 flex items-center justify-center">
+              <i class="fa-solid fa-list-check text-base md:text-3xl lg:text-4xl"></i>
+            </span>
             <span class="text-[7px] md:text-base lg:text-md xl:text-lg md:ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">Projects</span>
           </RouterLink>
           <RouterLink to="/contact"
             class="h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex flex-col md:flex-row items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <i class="fa-solid fa-envelope text-base md:text-3xl lg:text-4xl xl:text-5xl"></i>
+            <span class="md:w-12 md:h-12 flex items-center justify-center">
+              <i class="fa-solid fa-envelope text-base md:text-3xl lg:text-4xl"></i>
+            </span>
             <span class="text-[7px] md:text-base lg:text-md xl:text-lg md:ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">Contact</span>
           </RouterLink>
@@ -89,7 +100,7 @@ const toggleMenu = () => {
                 :class="{ 'opacity-0 md:opacity-0 md:w-0': !isOpen, 'text-[7px] opacity-100 md:opacity-100 md:w-auto': isOpen }">Language</span>
             </button>
             <div
-              class="fixed right-14 -top-8 md:top-20 md:left-0 w-24 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg overflow-hidden shadow-lg py-0 z-10 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition">
+              class="fixed right-16 -top-8 md:top-20 md:left-0 w-24 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg overflow-hidden shadow-lg py-0 z-10 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition">
               <a href="?lang=en"
                 class="block w-full text-left px-4 py-1 dark:hover:bg-gray-700 hover:bg-gray-200">English</a>
               <a href="?lang=kh"
@@ -109,16 +120,19 @@ const toggleMenu = () => {
             </span>
           </button>
         </nav>
-        <hr class="hidden md:flex border border-gray-400/50 mt-4">
+
+        <hr class="hidden md:flex border-1 border-gray-300 mt-4">
 
         <!-- Desktop Screen -->
         <div class="grid grid-rows-2 gap-2 md:mt-4">
           <button @click="toggleDark()"
             class="hidden md:flex h-14 md:h-20 bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex items-center justify-center transition"
             :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-            <span class="text-base md:text-3xl">
-              {{ isDark ? "🌜" : "🔆" }}
-            </span>
+            <div class="md:w-12 md:h-12 flex items-center justify-center">
+              <span class="text-base md:text-3xl">
+                {{ isDark ? "🌜" : "🔆" }}
+              </span>
+            </div>
             <span class="ml-2 transition-all duration-300"
               :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">
               {{ isDark ? "Dark" : "Light" }}
@@ -128,12 +142,14 @@ const toggleMenu = () => {
             <button
               class="h-full w-full bg-green-400/50 hover:bg-green-600/50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-white/80 font-medium rounded-xl flex items-center justify-center transition"
               :class="{ 'md:justify-start md:px-4': isOpen, 'md:justify-center': !isOpen }">
-              <i class="fa-solid fa-language text-base md:text-3xl"></i>
+              <span class="md:w-12 md:h-12 flex items-center justify-center">
+                <i class="fa-solid fa-language text-base md:text-3xl"></i>
+              </span>
               <span class="ml-2 transition-all duration-300"
                 :class="{ 'opacity-0 md:opacity-0 md:w-0 md:ml-0': !isOpen, 'opacity-100 md:opacity-100 md:w-auto': isOpen }">Language</span>
             </button>
             <div
-              class="absolute right-0 top-2 w-24 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg overflow-hidden shadow-lg py-0 z-10 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition">
+              class="fixed -right-20 bottom-36 w-24 bg-white dark:bg-gray-800 dark:text-gray-200 rounded-lg overflow-hidden shadow-lg py-0 z-10 invisible group-hover:visible opacity-0 group-hover:opacity-100 transition">
               <a href="?lang=en"
                 class="block w-full text-left px-4 py-1 dark:hover:bg-gray-700 hover:bg-gray-200">English</a>
               <a href="?lang=kh"
