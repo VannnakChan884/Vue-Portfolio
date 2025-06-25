bc3f7a8fda4d8002902a66ec57fa9a8b369c14c7
Chan Vannak <vannakchan884@gmail.com>
2025-06-25 21:41:50 +0700

    Modify fetch api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script setup>
   import { ref, onMounted } from 'vue'
   import axios from 'axios'
@@ -16,8 +15,6 @@
     }
   })
 </script>
-=======
->>>>>>> 5386990dc32ab0e8b30ca35ee36375f15b9d5150
 <template>
   <!-- About Section -->
   <div
@@ -28,8 +25,6 @@
         <!-- Education & Career -->
         <div class="md:w-1/2 mb-8 md:mb-0">
           <h3 class="text-xl dark:text-gray-200 font-semibold mb-4">Education & Career</h3>
-<<<<<<< HEAD
-
           <ul v-for="about in aboutList" :key="about.id" class="space-y-4 text-justify text-gray-700">
             <li class="flex items-start">
               <i class="fa-solid fa-graduation-cap dark:text-gray-200 mt-1 mr-3"></i>
@@ -47,79 +42,14 @@
                   <p>{{ exp.description }}</p>
                 </li>
               </ul>
-=======
-          <ul class="space-y-4 text-justify text-gray-700">
-            <li class="flex items-start">
-              <i class="fa-solid fa-graduation-cap dark:text-gray-200 mt-1 mr-3"></i>
-              <div>
-                <h4 class="dark:text-gray-200 font-medium">Education</h4>
-                <p class="dark:text-gray-300 font-light">Bachelor's Degree in Management Information Systems</p>
-              </div>
-            </li>
-            <li class="flex items-start">
-              <i class="fa-solid fa-briefcase dark:text-gray-200 mt-1 mr-3"></i>
-              <div>
-                <h4 class="font-medium dark:text-gray-200">
-                  experience_one
-                </h4>
-                <p class="font-light dark:text-gray-300">Provide design services for posters, logos, stickers,
-                  packaging... to customers.
-                </p>
-              </div>
-            </li>
-            <li class="flex items-start dark:text-gray-200">
-              <i class="fa-solid fa-briefcase mt-1 mr-3"></i>
-              <div>
-                <h4 class="font-medium">
-                  experience_two
-                </h4>
-                <p class="font-light dark:text-gray-300">Created posters for promoting products on social media.</p>
-              </div>
-            </li>
-            <li class="flex items-start dark:text-gray-200">
-              <i class="fa-solid fa-server mt-1 mr-3"></i>
-              <div>
-                <h4 class="font-medium">
-                  experience_three
-                </h4>
-                <p class="font-light dark:text-gray-300">Providing technical assistance and network solutions.</p>
-              </div>
-            </li>
-            <li class="flex items-start dark:text-gray-200">
-              <i class="fa-solid fa-server mt-1 mr-3"></i>
-              <div>
-                <h4 class="font-medium">
-                  current_job
-                </h4>
-                <p class="font-light dark:text-gray-300">Providing technical assistance and network solutions.</p>
-              </div>
->>>>>>> 5386990dc32ab0e8b30ca35ee36375f15b9d5150
             </li>
           </ul>
         </div>
 
         <!-- My Journey -->
-<<<<<<< HEAD
         <div v-for="about in aboutList" :key="about.id" class="md:w-1/2 md:pl-12 text-gray-700 text-justify">
           <h3 class="text-xl font-semibold dark:text-gray-200 mb-4">{{ about.title }}</h3>
           <p class="dark:text-gray-300 mb-4">{{ about.description }}</p>
-=======
-        <div class="md:w-1/2 md:pl-12 text-gray-700 text-justify">
-          <h3 class="text-xl font-semibold dark:text-gray-200 mb-4">My Journey</h3>
-          <p class="dark:text-gray-300 mb-4">
-            With a background in both design and information systems, I've cultivated a unique skill set that allows me
-            to bridge the gap between technical functionality and aesthetic appeal.
-          </p>
-          <p class="dark:text-gray-300 mb-4">
-            My journey began in graphic design, where I developed a keen eye for visual communication. This foundation
-            led me to explore the technical side of digital experiences, eventually specializing in IT support and web
-            development.
-          </p>
-          <p class="dark:text-gray-300">
-            Today, I combine these skills to create solutions that are not only technically sound but also visually
-            compelling and user-friendly.
-          </p>
->>>>>>> 5386990dc32ab0e8b30ca35ee36375f15b9d5150
         </div>
       </div>
     </div>
